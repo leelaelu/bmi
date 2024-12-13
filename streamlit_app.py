2504acaa86d896e5f24de627f9bb929c698d6e5e
leelaelu <20242524@sdhs.gwe.hs.kr>
2024-12-13 20:15:48 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -146,24 +146,20 @@ if st.button("Predict"):
 
 if prediction < 0.5: # 정상
     st.write("### Prediction Result: Low Price Segment")
-    st.image("https://via.placeholder.com/300", caption="Low Segment Image 1")
     st.video("https://www.youtube.com/watch?v=dQw4w9WgXcQ")  # YouTube 썸네일
-    st.text("추~~")
+    st.text("일반식을 추천합니다.")
 
 elif prediction >= 0.5: # 비만 1단계
     st.write("### Prediction Result: Low Price Segment")
-    st.image("https://via.placeholder.com/300", caption="Low Segment Image 1")
     st.video("https://youtu.be/swRNeYw1JkY?si=dlDjPClrWnlYhW37")  # YouTube 썸네일
-    st.text("추천ㅇㄴㄹ주 3회입니다.")
+    st.text("단백질식을 추천합니다.")
 
 elif prediction >= 3.5: # 비만 2단계
     st.write("### Prediction Result: Low Price Segment")
-    st.image("https://via.placeholder.com/300", caption="Low Segment Image 1")
     st.video("https://youtu.be/_ffhxHV630A?si=7CP9-pcRkZTrLQDv")  # YouTube 썸네일
-    st.text("추천 ㄴㅁㅇㄹ 주 4회입니다.")
+    st.text("채식을 추천합니다.")
 
 elif prediction >= 4.5: # 비만 3단계
     st.write("### Prediction Result: Low Price Segment")
-    st.image("https://via.placeholder.com/300", caption="Low Segment Image 1")
     st.video("https://youtu.be/LXTW1Nm_Z3Y?si=AFO2-AtPMEUSb5_O")  # YouTube 썸네일
-    st.text("추천 운ㅇㄹ주 5회입니다.")
+    st.text("저염식을 추천합니다.")
